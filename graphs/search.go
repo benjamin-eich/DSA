86154e877392b4fd4d4c7fb22dc143a7b4cd0062
Benjamin Eich <benjamin-eich@web.de>
2025-06-17 14:21:34 +0200

    implement depth-first search (DFS) algorithm and add corresponding unit tests

--- graphs/search.go
+++ graphs/search.go
@@ -30,3 +30,27 @@ func Bfs[T comparable](graph *Graph[T], start T) []T {
 
 	return order
 }
+
+func Dfs[T comparable](graph *Graph[T], start T, visited map[T]bool) []T {
+	if visited == nil {
+		visited = make(map[T]bool)
+	}
+
+	order := make([]T, 0)
+
+	if graph.Empty() {
+		return order
+	}
+
+	if _, ok := visited[start]; !ok {
+		visited[start] = true
+		order = append(order, start)
+
+		for _, neighbor := range graph.GetNeighbors(start) {
+			if _, ok := visited[neighbor]; !ok {
+				order = append(order, Dfs[T](graph, neighbor, visited)...)
+			}
+		}
+	}
+	return order
+}
--- graphs/search_test.go
+++ graphs/search_test.go
@@ -69,3 +69,58 @@ func TestBfsHandlesEmptyGraph(t *testing.T) {
 		t.Errorf("Bfs() = %v, want %v", order, expectedOrder)
 	}
 }
+
+func TestDfsReturnsNodesInDepthFirstOrder(t *testing.T) {
+	graph := getSearchTestGraph()
+	expectedOrder := []string{"A", "B", "D", "C", "E"}
+
+	order := Dfs(graph, "A", nil)
+
+	if !reflect.DeepEqual(order, expectedOrder) {
+		t.Errorf("Dfs() = %v, want %v", order, expectedOrder)
+	}
+}
+
+func TestDfsHandlesDisconnectedGraph(t *testing.T) {
+	graph := NewGraph[string]()
+	graph.AddEdge("A", "B")
+	graph.AddEdge("C", "D")
+	expectedOrder := []string{"A", "B"}
+
+	order := Dfs(graph, "A", nil)
+
+	if !reflect.DeepEqual(order, expectedOrder) {
+		t.Errorf("Dfs() = %v, want %v", order, expectedOrder)
+	}
+
+	expectedOrder = []string{"C", "D"}
+	order = Dfs(graph, "C", nil)
+
+	if !reflect.DeepEqual(order, expectedOrder) {
+		t.Errorf("Dfs() = %v, want %v", order, expectedOrder)
+	}
+}
+
+func TestDfsReturnsSingleNodeForIsolatedStart(t *testing.T) {
+	graph := NewGraph[string]()
+	graph.AddEdge("A", "B")
+	graph.AddEdge("C", "D")
+	expectedOrder := []string{"E"}
+
+	order := Dfs(graph, "E", nil)
+
+	if !reflect.DeepEqual(order, expectedOrder) {
+		t.Errorf("Dfs() = %v, want %v", order, expectedOrder)
+	}
+}
+
+func TestDfsHandlesEmptyGraph(t *testing.T) {
+	graph := NewGraph[string]()
+	expectedOrder := []string{}
+
+	order := Dfs(graph, "A", nil)
+
+	if !reflect.DeepEqual(order, expectedOrder) {
+		t.Errorf("Dfs() = %v, want %v", order, expectedOrder)
+	}
+}
